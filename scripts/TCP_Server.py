ecd4e110d8aa091f191bc16184616edd0ca57a0c
Kangyang Wu <kangyangwu@Kangyangs-Mac.local>
2019-12-26 11:31:13 +0100

    modified

--- scripts/TCP_Server.py
+++ scripts/TCP_Server.py
@@ -6,35 +6,36 @@ import threading
 def deal_client_request(ip_port, service_client_socket):
     print("客户端连接成功", ip_port)
 
-    file_name = service_client_socket.recv(1024)
-    file_name_data = file_name.decode("utf-8")
-    if os.path.exists(file_name_data):
-        fsize = os.path.getsize(file_name_data)
-        fmb = fsize/float(1024*1024)
-        send_data = "文件名: {} 文件大小: {:.2f}MB".format(file_name_data, fmb)
-        service_client_socket.send(send_data.encode("utf-8"))
-        print("请求文件名: {} 文件大小: {:.2f}MB".format(file_name_data, fmb))
-
-        options = service_client_socket.recv(1024)
-        if options.decode("utf-8") == 'y':
-            with open(file_name_data, 'rb') as f:
-                nums = fsize / 1024
-                cnum = 0
-
-                while True:
-                    file_data = f.read(1024)
-                    cnum = cnum + 1
-                    progress = cnum / nums * 100
-                    print("当前已下载：{:.2f}%".format(progress), end='\r')
-                    if file_data:
-                        service_client_socket.send(file_data)
-                    else:
-                        print("请求的文件数据发送完毕")
-                        break
-        else:
-            print("取消下载！")
+    file_list = os.listdir('../data')
+    send_data = ''
+    f_size = []
+    for file in file_list:
+        f_size.append(os.path.getsize(file))
+        fmb = f_size[-1]/float(1024*1024)
+        send_data += 'file name: {} file size: {:.4f}MB\n'.format(file, fmb)
+    if send_data:
+        service_client_socket.send(send_data.encode('utf-8'))
     else:
-        print("下载的文件不存在！")
+        service_client_socket.send('no file found!'.encode('utf-8'))
+    options = service_client_socket.recv(1024)
+    file_name = options.decode('utf-8')
+    if file_name in file_list:
+        with open(file_name, 'rb') as f:
+            nums = f_size[file_list.index(file_name)] / 1024
+            cnum = 0
+
+            while True:
+                file_data = f.read(1024)
+                cnum = cnum + 1
+                progress = cnum / nums * 100
+                print("当前已下载：{:.2f}%".format(progress), end='\r')
+                if file_data:
+                    service_client_socket.send(file_data)
+                else:
+                    print("请求的文件数据发送完毕")
+                    break
+    else:
+        print("can not find file！")
     service_client_socket.close()
 
 
